5bea11d271c657700640dfdf07660b4fb5334e28
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:22:48 -0300

    atv3

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿int valorCarro = 500000;
+double valorFinal = valorCarro + (valorCarro * 0.05) ;
+
+Console.WriteLine($"O valor da sua comissao é de: {valorCarro * 0.05}");
+Console.WriteLine($"A comissao do carro sera de: {valorFinal} ");
