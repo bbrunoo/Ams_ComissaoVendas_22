704078df2a00270263c696f99f7e1e9def786add
lucasxxtq <115241790+lucasxxtq@users.noreply.github.com>
2022-10-07 10:15:14 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,4 +6,7 @@ Console.WriteLine($"O valor da sua comissao é de: {valorCarro * 0.05}");
 Console.WriteLine($"A comissao do carro sera de: {valorFinal} ");
 if (n = 10){
   Console.WriteLine("Sou Foda");
+  n++:
 }
+
+Console.WriteLine("Fim da execução");
